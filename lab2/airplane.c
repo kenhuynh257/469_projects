fc3823bb3b61afbc7c2e619bbb44bd7a1e081893
Margaret R. McCarthy <mrmcc@uw.edu>
2016-04-08 12:33:12 -0700

    started C program

---
+++ lab2/airplane.c
@@ -0,0 +1,26 @@
+/* Write a function to prompt the user for the duration of a nonstop flight to
+ * London from Seattle, in hours, as a floating point number.
+ *
+ * Based upon the number of air miles from Seattle to London, compute, then
+ * write a second function print out the estimated velocity of the aircraft as
+ * a floating point number.
+ *
+ * Using the velocity you computed and an estimated head wind of 89.6 miles per
+ * hour, write a third function compute the estimated duration of the flight as
+ * a floating point number.
+ *
+ * Finally write a fourth function to print the estimated flight duration.
+ */
+ 
+ 
+int main(int argc, char *argv[]) {
+
+}
+
+estimateVelocity() {
+
+}
+
+estimateTime() {
+
+}
