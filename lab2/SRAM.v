63c26f707cd23b0e439be139df822f6152b01395
Maggie McCarthy <mrmcc@uw.edu>
2016-04-15 22:58:05 -0700

    simple SRAM

--- lab2/SRAM.v
+++ lab2/SRAM.v
@@ -1,6 +1,20 @@
-module SRAM();
+module SRAM(data, addr, weBar, oeBar);
+	inout [7:0] data;
+	input [7:0] addr;
+	input weBar;
+	input oeBar;
 
+	reg [7:0] state [255:0];
+
+	assign data[7:0] = oeBar ? 'bz : state[addr[7:0]][7:0];
+
+	always @(*) begin
+		if (!weBar & oeBar) begin
+			state[addr[7:0]][7:0] = data[7:0];
+		end
+	end
 endmodule
+
 
 module MAR(outAdd, inAdd, reset, clock);
 	output reg [7:0] outAdd;
