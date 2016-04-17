30a7c0bbf03eee0a6c1c58ab9a522fa02de28bf3
kenhuynh257 <kenhuynh257@gmail.com>
2016-04-17 13:49:44 -0700

    Update regfile.v

--- lab2/regfile.v
+++ lab2/regfile.v
@@ -1,35 +1,33 @@
 /*
 32x32 register 
 */
-model regfile( clk, rst, wrt_en, wrt_data, rd_s1, rd_s2, wrt_s, rd_data1, rd_data2)
+module regfile( clk, rst, wrt_en, wrt_data, rd_s1, rd_s2, wrt_s, rd_data1, rd_data2)
 	logic input clk, rst, wrt_en;
 	logic input [4:0]  wrt_s, rd_s1,rd_s2;
 	logic input [31:0] wrt_data;
 	logic output [31:0] rd_data1, rd_data2;
-	logic [31:0] regf;
+	logic [31:0] regf [31:0];
 	
 	integer i, j;
-	always@(posedge clk) begin
+	always_comb begin
 		
-		 if (and(wrt_en,wrt_s == 0))
-					regf[0] = 0;
-		else begin
-			for(i=0; i<32; i++) begin
+		for(j=0; j<32; j ++) begin
+		
+			if(rd_s1 ==j) rd_data1 = regf[j];
+			if(rd_s2 == j)rd_data2 = regf[j];
+	
+	
+		end
+	end	
+	regf[0]= 32'b0;
+	always@(posedge clk) begin
+			for(i=1; i<32; i++) begin
 				if (and(wrt_en, wrt_s == i)) 
 					regf[i] = wrt_data[i];
-				end	
+				
 			end	
 			
 		end
 		
-	always_comb begin
-		
-		for(j=0; j<32; j ++)
-	
-	
-	
-	
-	
-	
-	
-	end
+endmodule
+
