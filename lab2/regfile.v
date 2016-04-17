bf8e80f6db94b29e6a3ccb85af9f30eb2ecbe72b
kenhuynh257 <kenhuynh257@gmail.com>
2016-04-17 16:14:46 -0700

    Update regfile.v

--- lab2/regfile.v
+++ lab2/regfile.v
@@ -1,33 +1,47 @@
 /*
 32x32 register 
 */
-module regfile( clk, rst, wrt_en, wrt_data, rd_s1, rd_s2, wrt_s, rd_data1, rd_data2)
-	logic input clk, rst, wrt_en;
-	logic input [4:0]  wrt_s, rd_s1,rd_s2;
-	logic input [31:0] wrt_data;
-	logic output [31:0] rd_data1, rd_data2;
-	logic [31:0] regf [31:0];
+module regfile(clk, rst, wrt_en, wrt_data, rd_s1, rd_s2, wrt_s, rd_data1, rd_data2);
+	input  clk, rst, wrt_en;
+	input [4:0]  wrt_s, rd_s1,rd_s2;
+	input [31:0] wrt_data;
+	output reg [31:0] rd_data1, rd_data2;
+	reg [31:0] regf [31:0];
+	wire temp,temp2;
 	
 	integer i, j;
-	always_comb begin
+
+	always @(*) 
+	begin
+		regf[0] = 32'b0;
+		for(j=0; j<32; j ++) 
+		begin
 		
-		for(j=0; j<32; j ++) begin
-		
-			if(rd_s1 ==j) rd_data1 = regf[j];
-			if(rd_s2 == j)rd_data2 = regf[j];
-	
+			if(rd_s1 == j)
+			begin
+				rd_data1 = regf[j];
+			end
+			if(rd_s2 == j)
+			begin
+				rd_data2 = regf[j];
+			end
 	
 		end
 	end	
-	regf[0]= 32'b0;
-	always@(posedge clk) begin
-			for(i=1; i<32; i++) begin
-				if (and(wrt_en, wrt_s == i)) 
-					regf[i] = wrt_data[i];
-				
-			end	
-			
-		end
+	 
+	assign temp2 = (wrt_s == i);
+	and u1(temp,wrt_en, temp2);
+	always@(posedge clk ) 
+	begin
+		for(i=1; i<32; i++) 
+		begin
+			if (temp) 
+			begin
+				regf[i] <= wrt_data[i];
+			end
+		end	
+		
+	end
 		
 endmodule
 
