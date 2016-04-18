bab583f8a360331a5fa37b94c1969c89667ad5b0
kenhuynh257 <kenhuynh257@gmail.com>
2016-04-18 15:45:26 -0700

    Update regfile.v

--- lab2/regfile.v
+++ lab2/regfile.v
@@ -7,41 +7,46 @@ module regfile(clk, rst, wrt_en, wrt_data, rd_s1, rd_s2, wrt_s, rd_data1, rd_dat
 	input [31:0] wrt_data;
 	output reg [31:0] rd_data1, rd_data2;
 	reg [31:0] regf [31:0];
-	wire temp,temp2;
-	
+	wire [31:0] temp;
+	wire write; 
+	reg temp2;
 	integer i, j;
-
-	always @(*) 
-	begin
-		regf[0] = 32'b0;
-		for(j=0; j<32; j ++) 
-		begin
-		
-			if(rd_s1 == j)
-			begin
-				rd_data1 = regf[j];
-			end
-			if(rd_s2 == j)
-			begin
-				rd_data2 = regf[j];
-			end
 	
+	decoder d1 (wrt_s, temp);
+	
+	and u1(write,wrt_en, temp2);
+	always@(*) begin
+		for(j=1; i<32; j++) begin
+			temp2 = temp[j]; 
 		end
-	end	
+	end
 	 
-	assign temp2 = (wrt_s == i);
-	and u1(temp,wrt_en, temp2);
 	always@(posedge clk ) 
 	begin
 		for(i=1; i<32; i++) 
 		begin
-			if (temp) 
+			if (write) 
 			begin
 				regf[i] <= wrt_data[i];
 			end
 		end	
 		
+		// read the slected reg
+		rd_data1 = regf[rd_s1];	
+		rd_data2 = regf[rd_s2];
+		
+		// 1st reg is always 0
+		regf[0] = 0;
 	end
 		
 endmodule
 
+module decoder (in, out );
+	//input clk;
+	input  [4:0] in;
+	output  [31:0] out;
+	wire [31:0] out;
+	//always@(posedge clk)
+	assign out = 1<< in ;
+
+endmodule
