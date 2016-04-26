489f65f91a7d426c80f6e90f246cdc0bd3f1afe6
Margaret McCarthy <mrmcc@uw.edu>
2016-04-26 12:48:37 -0700

    orginization

--- lab2/DE1_SoC.v
+++ lab2/DE1_SoC.v
@@ -1,10 +1,10 @@
 // Top-level module that defines the I/Os for the DE-1 SoC board
-module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, CLOCK_50_50);
+module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, CLOCK_50);
 	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	output [9:0] LEDR;
 	input [3:0] KEY;
 	input [9:0] SW;
-	input CLOCK_50_50;
+	input CLOCK_50;
 	// Default values, turns off the HEX displays
 	assign HEX0 = 7'b1111111;
 	assign HEX1 = 7'b1111111;
