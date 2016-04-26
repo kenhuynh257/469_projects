22b4da2af8d19d7c1a5d7f17a12f5f732e77ef7f
Margaret McCarthy <mrmcc@uw.edu>
2016-04-26 13:48:52 -0700

    bhvl adder working

--- lab3/adder.v
+++ lab3/adder.v
@@ -1,13 +1,14 @@
 module adder(sum, cout, overf, a, b, cin, clock);
-  output [31:0] sum;
-  output cout, overf;
+  output reg [31:0] sum;
+  output reg cout;
+  output reg overf;
   input [31:0] a, b;
   input cin;
   input clock;
   
   always @(posedge clock) 
   begin
-    (cout, sum) = a + b + cin;
+    {cout, sum} = a + b + cin;
 	overf = (a[0] == b[0]);
   end
 endmodule
