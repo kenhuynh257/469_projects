127b50ceb39810f7bb3ca0d0475ebeec292e9868
rrreeeaaarrrhhh <mrmcc@uw.edu>
2016-04-27 11:33:59 -0700

    Update adder.v

--- lab3/adder.v
+++ lab3/adder.v
@@ -1,7 +1,7 @@
-module adder(sum, cout, overf, a, b, clock);
+module adder(sum, cout, overf, zerof, a, b, clock);
   output reg [31:0] sum;
   output reg cout;
-  output reg overf;
+  output reg overf, zerof;
   input [31:0] a, b;
   input clock;
   
@@ -9,5 +9,6 @@ module adder(sum, cout, overf, a, b, clock);
   begin
     {cout, sum} = a + b;
     overf = (a[0] == b[0]) && (a[0] != sum[0]);
+    zerof = !overf && (sum == 32'b0);
   end
 endmodule
