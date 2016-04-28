e7e696efe0219a6866baa67c8dd63a68d8baa37c
kenhuynh257 <kenhuynh257@gmail.com>
2016-04-28 16:43:47 -0700

    work now

--- lab3/orRTL.v
+++ lab3/orRTL.v
@@ -1,8 +1,8 @@
-module orRTL(a,b,out,zerof);
+module orRTL(a,b,out,zerof,negf,overf,carry);
 	input  [31:0] a, b;
 	output  [31:0] out;
-	output  zerof;
-
+	output  zerof,negf,overf,carry;
+	wire zerof;
 
 	genvar i;
 	 for (i = 0; i < 32; i = i + 1)
@@ -12,6 +12,9 @@ module orRTL(a,b,out,zerof);
 	end
 
 	assign zerof = (out == 0);
+	assign negf = 0;
+	assign overf = 0;
+	assign carry = 0;
 	
 endmodule
 
@@ -24,5 +27,3 @@ module or1(a,b,out);
 	assign out = a||b;
 	
 endmodule	
-
-
