cc8c31994e3bb0e7aec65b7332c78ac67f76d2e7
kenhuynh257 <kenhuynh257@gmail.com>
2016-04-28 15:55:29 -0700

    zerof zzzz

--- lab3/xorLogic.v
+++ lab3/xorLogic.v
@@ -1,25 +1,17 @@
-module xorLogic(a,b,out,control,clk);
+module xorLogic(a,b,out,zerof);
+	output zerof;
 	input  [31:0] a, b;
-	input  [2:0]control;
-	output reg [31:0] out;
-	reg [31:0]temp;
-	input clk;
-	parameter A = 3'b101;
+	output  reg[31:0] out;
+	
 	integer i;
 	always @(*) begin
-	for(i=0; i<32; i++) begin
-		if (control == A) 
-		begin
-		 temp[i] =   a[i]^b[i];
-		 end
-		else 
-		begin 
-		temp = 32'bx;
-		end
-		
-	end
-	end
 	
-	always @(posedge clk)
-		out = temp;
+	for(i=0; i<32; i++) begin
+		out[i] = a[i]^b[i];
+		
+		end
+	end
+	assign zerof = (out==0);
+	
+	
 endmodule
