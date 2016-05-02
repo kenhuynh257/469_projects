3705dd67e5c9e63f464243de083c7b43fd8694a4
kenhuynh257 <kenhuynh257@gmail.com>
2016-05-02 09:49:06 -0700

    Update andRTL.v

--- lab3/andRTL.v
+++ lab3/andRTL.v
@@ -3,13 +3,14 @@ module andRTL(a,b,out,zerof,negf,overf,carry);
 	output  [31:0] out;
 	output  zerof,negf,overf,carry;
 
-
+	generate
 	genvar i;
 	 for (i = 0; i < 32; i = i + 1)
 	 begin: andLogic
 			and1 o(a[i],b[i],out[i]);
 		
 	end
+	endgenerate
 	assign zerof = (out ==  32'b0);
 	assign negf = 0;
 	assign overf = 0;
