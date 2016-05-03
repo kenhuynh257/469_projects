668dd68024bad04e6a020d87a6680d06e751dcc9
davidgo25 <davidg25@uw.edu>
2016-05-03 10:09:09 -0700

    Added files via upload

---
+++ lab3/compare.v
@@ -0,0 +1,69 @@
+module compareTop();
+	wire [31:0] dataOut;
+	wire zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	wire [31:0]busA, busB;
+
+	compareBehave dut(busA, busB, dataOut[1:0], zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	tester testCompare(busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	initial begin
+		$dumpfile("compare.vcd");
+		$dumpvars(1, testCompare);
+	end
+
+endmodule
+
+
+module tester(busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] dataOut;
+	input zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	output reg [31:0] busA, busB;
+
+	parameter delay = 10;
+
+	initial begin 
+		$display("\t\t busA \t busB \t dataOut \t zeroFlag \t overflowFlag \t carryoutFlag \t negativeFlag \t time");
+		$monitor("\t\t %b \t %b \t %b \t %b \t %b \t %b \t %b \t %g",
+					busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag, $time);			
+	end
+
+	initial begin
+		busA = 0; busB = 0;
+		#delay;
+		busA = 0; busB = 1;
+		#delay;
+		busA = 1; busB = 0;
+		#delay;
+		busA = 1; busB = 1;
+		#delay;
+		#delay;
+	end
+endmodule
+
+
+module compareBehave(busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] busA, busB;
+	output reg[1:0] dataOut;
+	output reg zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	
+	always @(*) begin
+		zeroFlag = (busA == 0 && busB == 0) ? 1'b1 : 1'b0;
+		overflowFlag = 1'b0;
+		carryoutFlag = 1'b0;
+		negativeFlag = 1'b0;
+		if (busA == busB) dataOut = 2'b0;
+		else if (busA > busB) dataOut = 2'b01;
+		else dataOut = 2'b10;
+	end
+endmodule
+
+module compareRTL (busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] busA, busB;
+	output [1:0] dataOut;
+	output zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	
+	assign zeroFlag = (busA == 0 && busB == 0) ? 1'b1 : 1'b0;
+	assign overflowFlag = 1'b0;
+	assign carryoutFlag = 1'b0;
+	assign negativeFlag = 1'b0;
+	assign dataOut = (busA == busB) ? 2'b0 : (busA > busB) ? 2'b01 : 2'b10;
+endmodule
---
+++ lab3/shift.v
@@ -0,0 +1,96 @@
+module shiftTop();
+	wire [31:0] dataOut;
+	wire zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	wire [31:0]busA, busB;
+	wire [2:0] control;
+
+	shiftBehave dutShift(busA, busB[1:0], dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	tester testShift(busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+
+	initial begin
+		$dumpfile("shift.vcd");
+		$dumpvars(1, testShift);
+	end
+endmodule
+
+
+
+module tester(busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] dataOut;
+	input zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	output reg [31:0]busA, busB;
+
+	parameter delay = 10;
+
+	initial begin 
+		$display("\t\t busA \t busB \t dataOut \t zeroFlag \t overflowFlag \t carryoutFlag \t negativeFlag \t time");
+		$monitor("\t\t %b \t %b \t %b \t %b \t %b \t %b \t %b \t %g",
+					busA, busB, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag, $time);			
+	end
+
+	initial begin
+		busA = 32'b11110000111100001111000011110000;
+		busB = 32'b0;
+		#delay;
+		busB = 1;
+		#delay;
+		busB = 2;
+		#delay;
+		busB = 3;
+		#delay;
+		busB = 4;
+		#delay;
+	end
+endmodule
+
+ 
+module shiftBehave(in, amount, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] in;
+	input [1:0] amount;
+	output reg[31:0] dataOut;
+	output reg zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	
+	always @(*) begin
+		overflowFlag = 1'b0;
+		case (amount)
+			2'b00: begin 
+					carryoutFlag = 1'b0;
+					dataOut = in;
+					negativeFlag = dataOut[31];
+					zeroFlag = (dataOut == 0) ? 1'b1 : 1'b0;
+					end
+			2'b01: begin
+					carryoutFlag = in[31];
+					dataOut = in << 1;
+					negativeFlag = dataOut[31];
+					zeroFlag = (dataOut == 0) ? 1'b1 : 1'b0; // does the zero flag specify if the output or input is zero?
+					end
+			2'b10: begin
+					carryoutFlag = in[31];
+					dataOut = in << 2;
+					negativeFlag = dataOut[31];
+					zeroFlag = (dataOut == 0) ? 1'b1 : 1'b0;
+					end
+			2'b11: begin
+					carryoutFlag = in[31];
+					dataOut = in << 3;
+					negativeFlag = dataOut[31];
+					zeroFlag = (dataOut == 0) ? 1'b1 : 1'b0;
+					end
+		endcase
+	end
+endmodule
+
+module shiftRTL(in, amount, dataOut, zeroFlag, overflowFlag, carryoutFlag, negativeFlag);
+	input [31:0] in;
+	input [1:0] amount;
+	output [31:0] dataOut;
+	output zeroFlag, overflowFlag, carryoutFlag, negativeFlag;
+	
+	assign overflowFlag = 1'b0;
+	assign carryoutFlag = (amount == 2'b00) ? 1'b0 : in[31];
+	assign dataOut = (amount == 2'b00) ? in : (amount == 2'b01) ? in << 1 : (amount == 2'b10) ? in << 2 : in << 3;
+	assign negativeFlag = dataOut[31];
+	assign zeroFlag = (dataOut == 0) ? 1'b1 : 1'b0;
+
+endmodule
