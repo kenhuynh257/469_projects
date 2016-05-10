75f4bc61e3d805c0792711fcc538015f2dd99756
rrreeeaaarrrhhh <mrmcc@uw.edu>
2016-05-10 11:01:23 -0700

    added comments that describe code

--- lab4/new_functions.v
+++ lab4/new_functions.v
@@ -1,19 +1,34 @@
+//loads a selected word of memory from the SRAM an stores it in a selected register
+//$t = MEM[$s + offset]; advance_pc (4);
+//syntax: $t, offset($s)
 module loadWord();
 
 endmodule
 
+//stores a word from a selected register into a spot in the SRAM
+//MEM[$s + offset] = $t; advance_pc (4);
+//syntax: sw $t, offset($s)
 module storeWord();
 
 endmodule
 
+//jump to target address
+//PC = nPC; nPC = (PC & 0xf0000000) | (target << 2);
+//syntax: j target
 module jump();
 
 endmodule
 
+//jump to an address specified register s
+//PC = nPC; nPC = $s;
+//syntax: jr $s
 module jumpRegister();
 
 endmodule
 
+//branches if the provided value s is greater than t by the specified offset
+//if $s > $t advance_pc (offset << 2)); else advance_pc (4);
+//syntax: bgt $s, $t, offset
 module branchGreaterThan();
 
 endmodule
