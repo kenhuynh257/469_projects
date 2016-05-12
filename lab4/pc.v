bcf80a10a491e98b67bd198bdc855de5778fdb82
kenhuynh257 <kenhuynh257@gmail.com>
2016-05-12 14:12:21 -0700

    add program counter ??????

---
+++ lab4/pc.v
@@ -0,0 +1,12 @@
+//program counter
+module PC(pcout,pcin,clk, rst);
+	input [31:0]pcin;
+	input clk, rst;
+	output reg [31:0] pcout;
+	
+	always@(posedge clk) begin
+		if(rst) pcout <= 0;
+		else pcout <= pcin;
+	end
+	
+endmodule
