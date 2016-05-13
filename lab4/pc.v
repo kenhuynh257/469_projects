9ee0ee50af91594bd7a74210fa8d0d3988f4a060
kenhuynh257 <kenhuynh257@gmail.com>
2016-05-13 10:18:15 -0700

    add instructionMem

--- lab4/pc.v
+++ lab4/pc.v
@@ -1,15 +1,4 @@
-//program counter
-module PC(pcout,pcin,clk, rst);
-	input [31:0]pcin;
-	input clk, rst;
-	output reg [31:0] pcout;
-	
-	always@(posedge clk) begin
-		if(rst) pcout <= 0;
-		else pcout <= pcin+4;
-	end
-	
-endmodule
+
 
 //program couter
 module PC(pcout,pcin,clk, rst);
