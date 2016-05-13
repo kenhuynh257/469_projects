73a33edea17fa1b6249ec3bf60754f21bdaa5025
kenhuynh257 <kenhuynh257@gmail.com>
2016-05-13 10:17:51 -0700

    add instructionMemory

--- lab4/pc.v
+++ lab4/pc.v
@@ -10,3 +10,28 @@ module PC(pcout,pcin,clk, rst);
 	end
 	
 endmodule
+
+//program couter
+module PC(pcout,pcin,clk, rst);
+	input [31:0]pcin;
+	input clk, rst;
+	output reg [31:0] pcout;
+	
+	always@(posedge clk) begin
+		if(rst) pcout <= 0;
+		else pcout <= pcin+4;
+	end
+	
+endmodule
+
+module instructionMem(instruction, addr,clk,rst);
+	input[31:0] addr;
+	input clk, rst;
+	output [31:0] instruction;
+	
+	always@(posedge clk)begin
+	
+		instruction <= addr;
+	end
+
+endmodule
