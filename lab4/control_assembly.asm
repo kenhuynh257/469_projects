ea32ce92dd3718f4f9446d1b603fd7ea6a9b2749
rrreeeaaarrrhhh <mrmcc@uw.edu>
2016-05-16 21:48:54 -0700

    Should be good, I'll run it by a TA tomorrow

--- lab4/control_assembly.asm
+++ lab4/control_assembly.asm
@@ -1,48 +1,29 @@
-# Things to ask TA:
-# - Immediates?
-# - Pointers?
-# - Exit Status?
-# - Size of Instructions
-# - 32 bit SRAM
-
-int A = 7;
-int B = 5;
-int C = 2;
-int D = 4;
-int* dPtr = &D;
-if (A – B) > 3
-{
-C = 6;
-D = D << 2
-}
-else
-{
-C = C << 5;
-*dPtr = 7;
-} 
-
   .data
-A: .word 7
-B: .word 5
-C: .word 2
-D: .word 4
-#assign pointer to D
+A: .word 7          # addr 0
+B: .word 5          # addr 1
+C: .word 2          # addr 2
+D: .word 4          # addr 3
   .text
 main:
-  li $t2 3
-  lw $t3 A
-  lw $t4 B
-  lw $t5 C
-  lw $t6 D
-  sub $t7 $t3 $t4
-  bgt $t7 $t2 if
-  sll $t6 $t6 3
-  sll $t6 $t6 2
-  #update D from the pointer
-  j ext
-if: 
-  #set c to six
-  sll $t5 $t5 2
-  j ext
+  li $t2 3          # 0x4
+  lw $t3 A          # 0x8 
+  lw $t4 B          # 0xC
+  sub $t7 $t3 $t4   # 0x10
+  bgt $t7 $t2 if    # 0x14
+  
+  lw $t5 C          # 0x18
+  sll $t5 $t5 3     # 0x1C
+  sll $t5 $t5 2     # 0x20
+  si 7 D            # 0x24
+  sw $t5 C          # 0x28
+  j ext             # 0x2C
+
+if:
+  lw $t6 D          # 0x30
+  si 6 C            # 0x34
+  sll $t6 $t6 2     # 0x38
+  sw $t6 D          # 0x3C
+  j ext             # 0x40
+
 ext:
-  #loop
+  j ext             # 0x44
