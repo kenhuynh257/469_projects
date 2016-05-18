65987001fea4c11f9dd536ddcc0ed7647467eb29
rrreeeaaarrrhhh <mrmcc@uw.edu>
2016-05-17 22:19:40 -0700

    Update control_assembly.asm

--- lab4/control_assembly.asm
+++ lab4/control_assembly.asm
@@ -5,7 +5,7 @@ C: .word 2          # addr 2
 D: .word 4          # addr 3
   .text
 main:
-  li $t2 3          # 0x4
+  addi $t2 $zero 3  # 0x4
   lw $t3 A          # 0x8 
   lw $t4 B          # 0xC
   sub $t7 $t3 $t4   # 0x10
@@ -14,16 +14,17 @@ main:
   lw $t5 C          # 0x18
   sll $t5 $t5 3     # 0x1C
   sll $t5 $t5 2     # 0x20
-  si 7 D            # 0x24
-  sw $t5 C          # 0x28
-  j ext             # 0x2C
+  addi $t1 $zero 7  # 0x24
+  sw $t1 D          # 0x28
+  sw $t5 C          # 0x2C
+  j ext             # 0x30
 
 if:
-  lw $t6 D          # 0x30
-  si 6 C            # 0x34
-  sll $t6 $t6 2     # 0x38
-  sw $t6 D          # 0x3C
-  j ext             # 0x40
+  lw $t6 D          # 0x34
+  si 6 C            # 0x38
+  sll $t6 $t6 2     # 0x3C
+  sw $t6 D          # 0x40
+  j ext             # 0x44
 
 ext:
-  j ext             # 0x44
+  j ext             # 0x48
