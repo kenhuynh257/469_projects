1a87f75ce2e5de2bcc91b4f24d03f925281ae0c1
davidg25 <davidg25@uw.edu>
2016-05-17 20:42:28 -0700

    updated opcodes in instructiondecoder.v

--- lab4/instructiondecoder.v
+++ lab4/instructiondecoder.v
@@ -45,57 +45,172 @@ module controlunit(regDst,branch,memRead,memtoReg,memWrite,ALUSrc, regWrite,ALUO
 	input[5:0] instruction;
 	output reg regDst,branch,memRead,memtoReg,memWrite,ALUSrc, regWrite;
 	output reg [1:0]ALUOp;
+	parameter [5:0]
+	NOP =	6'b0,
+	AND =	6'b100100,
+	ANDI =	6'b1100,
+	OR =	6'b100101,
+	ORI =	6'b001101,
+	XOR =	6'b100110,
+	XORI =	6'b1110,
+	ADD =	6'b100000,
+	ADDI =	6'b1000,
+	SUB = 	6'b100010,
+	SLT =	6'b101010,
+	SLL =	6'b1,
+	J =		6'b10,
+	JR =	6'b1001,
+	BGT =	6'b111,
+	LW = 	6'b100011,
+	SW = 	6'b101011;
+
 	
+	
+	// Everything except ALUOp
 	always@(*) begin
-		//r-format
-		if(instruction == 0) begin
-			regDst=1;
-			ALUSrc=0;
-			memtoReg = 0;
-			regWrite=1;
-			memRead = 0;
-			memWrite =0;
-			branch = 0;
-			ALUOp=2'b10;
-		end
-		//LW
-		else if(instruction == 6'b100011) begin
-			regDst=0;
-			ALUSrc=1;
-			memtoReg = 1;
-			regWrite=1;
-			memRead = 1;
-			memWrite =0;
-			branch = 0;
-			ALUOp=2'b00;
-		
-		end
-		//SW
-		else if (instruction == 6'b101011) begin
-			regDst= 1'bx;
-			ALUSrc=1;
-			memtoReg = 1'bx;
-			regWrite=0;
-			memRead = 0;
-			memWrite =1;
-			branch = 0;
-			ALUOp=2'b00;
-			
-		end
-		//bgt
-		else if(instruction == 6'b000111) begin
-			regDst=1'bx;
-			ALUSrc=0;
-			memtoReg = 1'bx;
-			regWrite=0;
-			memRead = 0;
-			memWrite =0;
-			branch = 1;
-			ALUOp=2'b01;
-	
-		end
-		//j and jr
-	end
+		case(instruction)
+			NOP: begin
+				end
+			AND: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			ANDI: begin
+				end
+			OR: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			ORI: begin
+				end
+			XOR: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			XORI: begin
+				end
+			ADD: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			ADDI: begin
+				end
+			SUB: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			SLT: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			SLL: begin
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+				end
+			J: begin
+				end
+			JR: begin
+				end
+			BGT: begin
+					regDst=1'bx;
+					ALUSrc=0;
+					memtoReg = 1'bx;
+					regWrite=0;
+					memRead = 0;
+					memWrite =0;
+					branch = 1;
+					ALUOp=2'b01;
+				end
+			LW: begin
+					regDst=0;
+					ALUSrc=1;
+					memtoReg = 1;
+					regWrite=1;
+					memRead = 1;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b00;
+				end
+			SW: begin
+					regDst= 1'bx;
+					ALUSrc=1;
+					memtoReg = 1'bx;
+					regWrite=0;
+					memRead = 0;
+					memWrite =1;
+					branch = 0;
+					ALUOp=2'b00;
+				end
+			default: begin // default is same as R function
+					regDst=1;
+					ALUSrc=0;
+					memtoReg = 0;
+					regWrite=1;
+					memRead = 0;
+					memWrite =0;
+					branch = 0;
+					ALUOp=2'b10;
+					j=0;
+					jr=0;
+					end
+		endcase
+
 	
 endmodule 
 
