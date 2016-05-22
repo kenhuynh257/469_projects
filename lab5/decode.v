7215f88b4094d98e555e6dbf368dff57c5de426b
Margaret R. McCarthy <mrmcc@uw.edu>
2016-05-22 15:25:21 -0700

    added skeleton for decode

--- lab5/decode.v
+++ lab5/decode.v
@@ -1,3 +1,8 @@
-module decode
-
+module decode (pcout, readDatat1, readDatat2, signExtend, inst2016, inst1511, RegWrite, wbControl, mControl, exControl, instruction, pcin);
+	output [7:0] pcout;
+	output [31:0] readDatat1, readDatat2, signExtend;
+	output [3:0] inst2016, inst1511;
+	output wbControl, mControl, exControl, RegWrite;
+	input [31:0] instruction;
+	input [6:0] pcin;
 endmodule
