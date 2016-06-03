e48566d1ee7e01f4a1119693c2122ddf2588eb25
davidg25 <davidg25@uw.edu>
2016-06-03 00:51:33 -0700

    working on fetch testbench, going to finish at home

--- lab5/fetch.v
+++ lab5/fetch.v
@@ -1,17 +1,23 @@
 module fetch (instruction, PCSrc, clock, reset, jumpAddr, jrAddr, branchAddr, pcWrite, IFFlush);
 	output reg [31:0] instruction;
-	input clock, reset;
+	input clock, reset; // reset doesn't do anything right now...
 	input [2:0] PCSrc; // one-hot encoded mux control to PC choosing between j, jr, and branch
 	input pcWrite; // enables writing to the PC for hazard control (stalling)
 	input IFFlush; // signal to flush the IF/ID pipeline register
 	input [6:0] jumpAddr, jrAddr, branchAddr;
-	wire [6:0] pcIn, pcOut;
-	always @(*) begin
+	reg [6:0] pcIn;
+	wire [6:0] pcOut;
+	wire [31:0] memoryOut;
+	reg [6:0] plusOne;
+	
+	
+	always @(PCSrc) begin
+		plusOne = pcOut + 1;
 		case(PCSrc)
-			1XX: pcIn = branchAddr;
-			01X: pcIn = jumpAddr;
-			001: pcIn = jrAddr;
-			000: pcIn = pcOut + 1;
+			3'b1XX: pcIn = branchAddr;
+			3'b01X: pcIn = jumpAddr;
+			3'b001: pcIn = jrAddr;
+			3'b000: pcIn = plusOne;
 			default: pcIn = 0;
 		endcase
 	end
@@ -40,6 +46,11 @@ module pc(pcWrite, pcIn, pcOut, clock);
 		end else begin
 			pcOut <= pcOut;
 		end
+	end
+	
+	initial begin
+		pcOut = 7'b0;
+	end
 endmodule
 
 module instructionMemory(address, instruction);
@@ -53,3 +64,35 @@ module instructionMemory(address, instruction);
 	// load instructions into memory
 	end
 endmodule
+
+module testbench();
+	wire [31:0] instruction;
+	wire [2:0] PCSrc;
+	wire clock, reset;
+	wire [6:0] jumpAddr, jrAddr, branchAddr;
+	wire pcWrite, IFFlush;
+	
+	fetch dut(instruction, PCSrc, clock, reset, jumpAddr, jrAddr, branchAddr, pcWrite, IFFlush);
+	tester test(instruction, PCSrc, clock, reset, jumpAddr, jrAddr, branchAddr, pcWrite, IFFlush);
+	
+	initial begin
+		$dumpfile("fetch.vcd");
+		$dumpvars(1, test);
+	end
+end
+
+module tester(instruction, PCSrc, clock, reset, jumpAddr, jrAddr, branchAddr, pcWrite, IFFlush);
+	input [31:0] instruction;
+	output reg [2:0] PCSrc;
+	output reg clock, reset;
+	output reg [6:0] jumpAddr, jrAddr, branchAddr;
+	output reg pcWrite, IFFlush;
+	
+	parameter delay = 10;
+	
+	initial begin
+	
+	end
+	
+
+	
