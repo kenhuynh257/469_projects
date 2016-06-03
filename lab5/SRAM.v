9d21f927e0b8f7c2fb48994d1892101c2fb9c05f
davidg25 <davidg25@uw.edu>
2016-06-03 00:05:05 -0700

    fetch is halfway done, still doing hazard stuff

--- lab5/SRAM.v
+++ lab5/SRAM.v
@@ -21,46 +21,13 @@
 // 2048x16 bit memory that can be read or written to. 'addr' selects
 // which of the 2048 words is to be written/read, and then either that
 // word is read by the MDR or the word in the MDR is written to that address.
-module SRAM(writeData, readData, address, memWrite, memRead, clock, memory0, memory1, memory2, memory3,
-memory4, memory5, memory6, memory7, memory8, memory9, memory10, memory11, memory12, memory13, memory14, memory15);
+module SRAM(writeData, readData, address, memWrite, memRead, clock, );
 	input [15:0] writeData;
 	input [31:0] address;
 	input memWrite, memRead, clock;
 	output [15:0] readData;
 	
-	output [15:0] memory0;
-	output [15:0] memory1;
-	output [15:0] memory2;
-	output [15:0] memory3;
-	output [15:0] memory4;
-	output [15:0] memory5;
-	output [15:0] memory6;
-	output [15:0] memory7;
-	output [15:0] memory8;
-	output [15:0] memory9;
-	output [15:0] memory10;
-	output [15:0] memory11;
-	output [15:0] memory12;
-	output [15:0] memory13;
-	output [15:0] memory14;
-	output [15:0] memory15;
-	
-	assign memory0 [15:0] = memory[0][15:0];
-	assign memory1 [15:0] = memory[1][15:0];
-	assign memory2 [15:0] = memory[2][15:0];
-	assign memory3 [15:0] = memory[3][15:0];
-	assign memory4 [15:0] = memory[4][15:0];
-	assign memory5 [15:0] = memory[5][15:0];
-	assign memory6 [15:0] = memory[6][15:0];
-	assign memory7 [15:0] = memory[7][15:0];
-	assign memory8 [15:0] = memory[8][15:0];
-	assign memory9 [15:0] = memory[9][15:0];
-	assign memory10 [15:0] = memory[10][15:0];
-	assign memory11 [15:0] = memory[11][15:0];
-	assign memory12 [15:0] = memory[12][15:0];
-	assign memory13 [15:0] = memory[13][15:0];
-	assign memory14 [15:0] = memory[14][15:0];
-	assign memory15 [15:0] = memory[15][15:0];
+
 	
 	reg [15:0] memory [31:0];
 	
@@ -84,3 +51,43 @@ memory4, memory5, memory6, memory7, memory8, memory9, memory10, memory11, memory
 		memory[5][15:0] = 32'd4;
 	end
 endmodule 
+
+/* This code can be used to see the contents of the memory in gtkwave
+
+memory0, memory1, memory2, memory3,
+memory4, memory5, memory6, memory7, memory8, memory9, memory10, memory11, memory12, memory13, memory14, memory15
+
+output [15:0] memory0;
+output [15:0] memory1;
+output [15:0] memory2;
+output [15:0] memory3;
+output [15:0] memory4;
+output [15:0] memory5;
+output [15:0] memory6;
+output [15:0] memory7;
+output [15:0] memory8;
+output [15:0] memory9;
+output [15:0] memory10;
+output [15:0] memory11;
+output [15:0] memory12;
+output [15:0] memory13;
+output [15:0] memory14;
+output [15:0] memory15;
+
+assign memory0 [15:0] = memory[0][15:0];
+assign memory1 [15:0] = memory[1][15:0];
+assign memory2 [15:0] = memory[2][15:0];
+assign memory3 [15:0] = memory[3][15:0];
+assign memory4 [15:0] = memory[4][15:0];
+assign memory5 [15:0] = memory[5][15:0];
+assign memory6 [15:0] = memory[6][15:0];
+assign memory7 [15:0] = memory[7][15:0];
+assign memory8 [15:0] = memory[8][15:0];
+assign memory9 [15:0] = memory[9][15:0];
+assign memory10 [15:0] = memory[10][15:0];
+assign memory11 [15:0] = memory[11][15:0];
+assign memory12 [15:0] = memory[12][15:0];
+assign memory13 [15:0] = memory[13][15:0];
+assign memory14 [15:0] = memory[14][15:0];
+assign memory15 [15:0] = memory[15][15:0];
+*/
--- lab5/fetch.v
+++ lab5/fetch.v
@@ -1,13 +1,52 @@
-module fetch (instruction, PCSrc, clock, reset, jumpAddr, pcWrite, IFWrite, IFFlush);
+module fetch (instruction, PCSrc, clock, reset, jumpAddr, jrAddr, branchAddr, pcWrite, IFWrite, IFFlush);
+	output reg [31:0] instruction;
+	input clock, reset;
+	input [2:0] PCSrc; // one-hot encoded mux control to PC choosing between j, jr, and branch
+	input pcWrite; // enables writing to the PC for hazard control (stalling)
+	input IFWrite; // enables writing to the IF/ID pipeline register
+	input IFFlush; // signal to flush the IF/ID pipeline register
+	input [6:0] jumpAddr, jrAddr, branchAddr;
+		
+	always @(*) begin
+		case(PCSrc)
+			1XX: pcIn = branchAddr;
+			01X: pcIn = jumpAddr;
+			001: pcIn = jrAddr;
+			000: pcIn = pcOut + 1;
+			default: pcIn = 0;
+		endcase
+	end
+	
+	pc programCounter(pcWrite, pcIn, pcOut, clock);
+	instructionMemory instructionMemory1(pcOut, memoryOut);
+	
+	always @(posedge clock) begin
+		instruction <= memoryOut;
+	end
+endmodule
+
+module pc(pcWrite, pcIn, pcOut, clock);
+	input clock;
+	input pcWrite; // used to freeze the PC address for hazard control
+	input [6:0] pcIn; // instruction address
+	output reg [6:0] pcOut;
+	
+	always @(posedge clock) begin
+		if (pcWrite) begin
+			pcOut <= pcIn;
+		end else begin
+			pcOut <= pcOut;
+		end
+endmodule
+
+module instructionMemory(address, instruction);
+	input [6:0] address;
 	output [31:0] instruction;
- 	input PCSrc, clock, reset, pcWrite, IFWrite, IFFlush;
-	input [6:0] jumpAddr;
-endmodule
-
-module pc();
-
-endmodule
-
-module instructionMemory();
-
+	reg [31:0] memory [127:0];
+	
+	assign instruction = memory[address[6:0]][31:0];
+	
+	initial begin
+	// load instructions into memory
+	end
 endmodule
