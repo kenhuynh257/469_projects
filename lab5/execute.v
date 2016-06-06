32aabd61e85968079ca23cf661c8cb94d457af64
Margaret R. McCarthy <mrmcc@uw.edu>
2016-06-05 22:33:09 -0700

    added timing

--- lab5/execute.v
+++ lab5/execute.v
@@ -2,10 +2,10 @@
 `include "ALUcontrol.v"
 `include "ALU_functions.v"
 
-module execute(ALUresult, busBpreMux, zeroF, regWriteSel, rt_DX, rd_DX, immediate, readData1, readData2, nextOutput, address, regDst, ALUSrc, ALUOp, clock);
-	output [31:0] ALUresult, busBpreMux;
-	output zeroF;
-	output [4:0] regWriteSel;
+module execute(ALUresult, outB, zeroF, regWriteSel, rt_DX, rd_DX, immediate, readData1, readData2, nextOutput, address, regDst, ALUSrc, ALUOp, clock);
+	output reg [31:0] ALUresult, outB;
+	output reg zeroF;
+	output reg [4:0] regWriteSel;
 	input [4:0] rt_DX, rd_DX;
 	input [31:0] immediate, readData1, readData2, nextOutput, address;
 	input [1:0] forwardA, forwardB;
@@ -13,20 +13,29 @@ module execute(ALUresult, busBpreMux, zeroF, regWriteSel, rt_DX, rd_DX, immediat
 	input [2:0] ALUOp;
 	input clock;
 	
-	wire [31:0] busA, busBpreMux, busB;
-	wire oFlag, nFlag, cFlag;
+	wire [31:0] busA, busBpreMux, busB, ALUout;
+	wire zFlag, oFlag, nFlag, cFlag;
 	wire [2:0] control;
+	wire [4:0] selTemp;
 	
 	mux32_4 mux1(busA, readData1, address, nextOutput, 32'b0, forwardA[0], forwardA[1]);
 	mux32_4 mux2(busBpreMux, readData2, address, nextOutput, 32'b0, forwardB[0], forwardB[1]);
 	
 	mux32_2 mux3(busB, busBpreMux, immediate, ALUSrc);
 	
-	alu arithmetic(ALUresult, zeroF, oFlag, nFlag, cFlag, busA, busB, control);
+	alu arithmetic(ALUout, zFlag, oFlag, nFlag, cFlag, busA, busB, control);
 	
 	ALUcontrol aCon(control, immediate[5:0], ALUOp);
 	
-	mux5_2to1 mux4(regWriteSel, rt_DX, rd_DX, regDst);
+	mux5_2to1 mux4(selTemp, rt_DX, rd_DX, regDst);
+	
+	always @ (posedge clock)
+	begin
+		regWriteSel <= selTemp;
+		zeroF <= zFlag;
+		ALUresult <= ALUout;
+		outB <= busBpreMux;
+	end
 	
 endmodule
 
