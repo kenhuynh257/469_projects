f5647e6eefdbbcb5a497e6897481c1b7cd90809b
davidg25 <davidg25@uw.edu>
2016-06-05 21:44:13 -0700

    memory finished, not tested but so simple

--- lab5/memory.v
+++ lab5/memory.v
@@ -1,11 +1,13 @@
 `include "SRAM.v"
-// NOT TESTED YET
+// not tested but it's just the SRAM which I think has been tested and
+// single wire or flip flop connections.
 module memory(
 	output branchSrc, /* 3rd bit of PCSrc */
-	output reg [31:0] arithmeticOut,
 	output reg [31:0] readData,
-	output [31:0] addressOut,
-	output reg [4:0] regWriteSelOut,
+	output reg [31:0] arithmeticOut,
+	output reg [4:0] MEMWBregSelOut,
+	output [4:0] regSelOut,
+	output [31:0] forwardALUResult,
 	input branchCtrl,
 	input zeroFlag,
 	input memWrite,
@@ -15,7 +17,8 @@ module memory(
 	input [31:0] writeData,
 	input clock);
 	
-	assign addressOut = addressIn;
+	assign regSelOut = regWriteSel;
+	assign forwardALUResult = addressIn;
 	assign branchSrc = branchCtrl & zeroFlag;
 	wire [31:0] memoryOut;
 	SRAM dataMemory(writeData, memoryOut, addressIn, memWrite, memRead, clock);
@@ -23,7 +26,65 @@ module memory(
 	always @(posedge clock) begin
 		readData <= memoryOut;
 		arithmeticOut <= addressIn;
-		regWriteSelOut <= regWriteSel;
+		MEMWBregSelOut <= regWriteSel;
 	end
 endmodule
+	
+module testbench();
+	wire branchSrc, /* 3rd bit of PCSrc */
+	wire [31:0] readData,
+	wire [31:0] arithmeticOut,
+	wire [4:0] MEMWBregSelOut,
+	wire [4:0] regSelOut,
+	wire [31:0] forwardALUResult,
+	wire branchCtrl,
+	wire zeroFlag,
+	wire memWrite,
+	wire memRead,
+	wire [31:0] addressIn,
+	wire [4:0] regWriteSel,
+	wire [31:0] writeData,
+	wire clock);
+	
+	memory MEM(branchSrc, readData, arithmeticOut, MEMWBregSelOut, regSelOut, forwardLUResult, 
+				branchCtrl, zeroFlag, memWrite, memRead, addressIn, regWriteSel, writeData, clock);
+	
+	tester test(branchCtrl, zeroFlag, memWrite, memRead, addressIn, regWriteSel, writeData, clock);
+	
+	initial begin
+		$dumpfile("IFID.vcd");
+		$dumpvars();
+	end
+endmodule
+
+module tester(
+	output reg branchCtrl,
+	output reg zeroFlag,
+	output reg memWrite,
+	output reg memRead,
+	output reg addressIn,
+	output reg regWriteSel,
+	output reg writeData,
+	output reg clock);
+	
+	integer i;
+	
+	parameter delay = 10;
+	
+	initial begin
+		// test writing memory
+		// reading memory
+	end
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
